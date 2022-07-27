48f74dd8af87b988200ceeeb1753c1bd16f09b06
dlrwtul <dibrahimalang@gmail.com>
2022-07-27 01:20:18 +0000

    t

--- src/Validator/MenuDoublonsValidator.php
+++ src/Validator/MenuDoublonsValidator.php
@@ -8,7 +8,6 @@ class MenuDoublonsValidator
 {
     public static function validate($object, ExecutionContextInterface $context, $payload)
     {
-        dd($object);
         if (self::checkDoublons($object,"getMenuBurgers","getBurger") ) {
             $context->buildViolation("il y'a des Burgers identiques")
             ->atPath('menuBurgers')
