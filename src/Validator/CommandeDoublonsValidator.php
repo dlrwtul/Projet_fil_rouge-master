c08958f174c3e792d03f3c6468b8717c41d6eca0
dlrwtul <dibrahimalang@gmail.com>
2022-08-04 11:10:29 +0000

    change controll access

--- src/Validator/CommandeDoublonsValidator.php
+++ src/Validator/CommandeDoublonsValidator.php
@@ -42,6 +42,7 @@ class CommandeDoublonsValidator
     public static function checkDoublons($object,string $commandeTruck,string $truck) {
         if (count($object->$commandeTruck()) != 0) {
             foreach ($object->$commandeTruck() as $key => $value) {
+                dd($value->$truck());
                 $id = $value->$truck()->getId();
                 foreach ($object->$commandeTruck() as $key2 => $value2) {
                     $id2 = $value2->$truck()->getId();
