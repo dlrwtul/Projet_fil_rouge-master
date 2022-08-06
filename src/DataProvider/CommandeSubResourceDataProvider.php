1e3c0deead83341bf18727c4aa4b55bf34738ead
dlrwtul <dibrahimalang@gmail.com>
2022-08-06 09:35:40 +0000

    commande client

--- src/DataProvider/CommandeSubResourceDataProvider.php
+++ src/DataProvider/CommandeSubResourceDataProvider.php
@@ -2,23 +2,35 @@
 
 namespace App\DataProvider;
 
+use App\Entity\Commande;
+use App\Repository\UserRepository;
 use ApiPlatform\Core\DataProvider\RestrictedDataProviderInterface;
 use ApiPlatform\Core\DataProvider\SubresourceDataProviderInterface;
-use App\Entity\Commande;
+use App\Repository\ClientRepository;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 
 class CommandeSubResourceDataProvider implements RestrictedDataProviderInterface, SubresourceDataProviderInterface
 {
     private $alreadyInvoked = false;
     private $subresourceDataProvider;
+    private $tokenStorage;
+    private $clientRepo;
 
-    public function __construct(SubresourceDataProviderInterface $subresourceDataProvider)
+    public function __construct(SubresourceDataProviderInterface $subresourceDataProvider,TokenStorageInterface $tokenStorage,ClientRepository $clientRepo)
     {
+        $this->tokenStorage = $tokenStorage;
         $this->subresourceDataProvider = $subresourceDataProvider;
+        $this->clientRepo = $clientRepo;
     }
 
     public function getSubresource(string $resourceClass, array $identifiers, array $context, string $operationName = null)
     {
-        dd($identifiers);
+        $user = $this->tokenStorage->getToken()->getUser();
+        $client = $this->clientRepo->findOneBy(["login" => $user->getUserIdentifier()]);
+        $identifiers["id"]["id"] = $client->getId();
+
+        //dd($identifiers);
+
         $this->alreadyInvoked = true;
 
         $collection = $this->subresourceDataProvider->getSubresource($resourceClass, $identifiers, $context);
