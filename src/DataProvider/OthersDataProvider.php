49d3d656181254d576f1fada17240876e0c162b0
dlrwtul <dibrahimalang@gmail.com>
2022-08-14 14:59:12 +0000

    quartier

--- src/DataProvider/OthersDataProvider.php
+++ src/DataProvider/OthersDataProvider.php
@@ -27,13 +27,13 @@ final class OthersDataProvider implements ItemDataProviderInterface, RestrictedD
         return Zone::class === $resourceClass || Quartier::class === $resourceClass || Taille::class === $resourceClass || Burger::class === $resourceClass || PortionFrites::class == $resourceClass;
     }
 
-    public function getItem(string $resourceClass, $id, string $operationName = null, array $context = []): Zone|Quartier|Taille|null
-    {
-        // Retrieve the blog post item from somewhere then return it or null if not found
-        $manager = $this->managerRegistery->getManagerForClass($resourceClass);
-        $repository = $manager->getRepository($resourceClass);
-        return  $repository->findOneBy(array('id' => $id));
-    }
+    // public function getItem(string $resourceClass, $id, string $operationName = null, array $context = []): Zone|Quartier|Taille|null
+    // {
+    //     // Retrieve the blog post item from somewhere then return it or null if not found
+    //     $manager = $this->managerRegistery->getManagerForClass($resourceClass);
+    //     $repository = $manager->getRepository($resourceClass);
+    //     return  $repository->findOneBy(array('id' => $id));
+    // }
 
     public function getCollection(string $resourceClass, string $operationName = null): ?array {
 
