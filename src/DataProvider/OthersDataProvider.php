d8d14c41d3eee8dbc77a10d0730722d63ff6a420
dlrwtul <dibrahimalang@gmail.com>
2022-08-18 09:29:48 +0000

    filter

--- src/DataProvider/OthersDataProvider.php
+++ src/DataProvider/OthersDataProvider.php
@@ -37,7 +37,9 @@ final class OthersDataProvider implements  RestrictedDataProviderInterface ,Coll
 
         $manager = $this->managerRegistery->getManagerForClass($resourceClass);
         $repository = $manager->getRepository($resourceClass);
-
+        if ($resourceClass == Livreur::class) {
+            dd($repository->findBy(array('isEtat' => true)));
+        }
         return $repository->findBy(array('isEtat' => true));
 
     }
